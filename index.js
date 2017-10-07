7706242fee231d0f509d0576f82672ade6645e52
dvejmz <dvejmz@gmail.com>
2017-10-07 17:46:53 +0100

    Add basic editorconfig linter

--- index.js
+++ index.js
@@ -9,51 +9,51 @@ app.set('view engine', 'pug');
 
 app.use(function (req, res) {
 
-    console.log(req.path);
+  console.log(req.path);
 
-    vault.get(req.path, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.redirect('back');
-            return;
+  vault.get(req.path, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.redirect('back');
+      return;
+    }
+
+    if (result.type === 'secrets') {
+      res.render('entries', {
+        entries: result.content,
+        backLink: req.get('Referrer')
+      });
+
+      return;
+    }
+
+    if (result.type === 'directory') {
+      let list = result.content;
+      // Remove table headers
+      list = list.slice(2);
+      const originalUrl = req.url.split('/').filter(el => { return el; });
+
+      if (originalUrl) {
+        let backLink = '';
+        if (originalUrl.length > 1) {
+          backLink = originalUrl;
+          backLink.pop();
+          backLink = backLink.join('/');
         }
 
-        if (result.type === 'secrets') {
-            res.render('entries', {
-                entries: result.content,
-                backLink: req.get('Referrer')
-            });
+        list.unshift({
+          name: '../',
+          link: [req.baseUrl, backLink].join('/')
+        });
+      }
 
-            return;
-        }
-
-        if (result.type === 'directory') {
-            let list = result.content;
-            // Remove table headers
-            list = list.slice(2);
-            const originalUrl = req.url.split('/').filter(el => { return el; });
-
-            if (originalUrl) {
-                let backLink = '';
-                if (originalUrl.length > 1) {
-                    backLink = originalUrl;
-                    backLink.pop();
-                    backLink = backLink.join('/');
-                }
-
-                list.unshift({
-                    name: '../',
-                    link: [req.baseUrl, backLink].join('/')
-                });
-            }
-
-            // Render directories view
-            res.render('list', { list: list });
-            return;
-        }
-    });
+      // Render directories view
+      res.render('list', { list: list });
+      return;
+    }
+  });
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
--- src/vault.js
+++ src/vault.js
@@ -3,46 +3,46 @@
 const cp = require('child_process');
 
 module.exports = () => {
-    const exec = cp.exec;
-    const env_variables = 'PATH='+process.env.PATH;
+  const exec = cp.exec;
+  const env_variables = 'PATH='+process.env.PATH;
 
-    return {
-        get
-    };
+  return {
+    get
+  };
 
-    function get(path, cb) {
-        const cmd = `${env_variables} ./vlt ${path}`;
-        exec(cmd, (err, stdout, stderr) => {
-            if (err) {
-                return cb(err, null);
-            }
+  function get(path, cb) {
+    const cmd = `${env_variables} ./vlt ${path}`;
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        return cb(err, null);
+      }
 
-            if (stdout.indexOf('Keys') !== 0) {
-                // Render secrets view
-                return cb(
-                    null,
-                    {
-                        type: 'secrets',
-                        content: stdout
-                    }
-                );
-            }
+      if (stdout.indexOf('Keys') !== 0) {
+        // Render secrets view
+        return cb(
+          null,
+          {
+            type: 'secrets',
+            content: stdout
+          }
+        );
+      }
 
-            const list = stdout.split('\n')
-                .filter((key) => {
-                    return key;
-                })
-                .map((key)=> {
-                    return { name: key, link: key };
-                });
-
-            return cb(
-                null,
-                {
-                    type: 'directory',
-                    content: list
-                }
-            );
+      const list = stdout.split('\n')
+        .filter((key) => {
+          return key;
+        })
+        .map((key)=> {
+          return { name: key, link: key };
         });
-    }
+
+      return cb(
+        null,
+        {
+          type: 'directory',
+          content: list
+        }
+      );
+    });
+  }
 };
