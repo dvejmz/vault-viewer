bc4bdf7f97edd97f0e338d03f64885ce99120ae8
dvejmz <dvejmz@gmail.com>
2018-05-10 21:29:56 +0100

    Remove vlt shell script, move functionality into vault-viewer

--- src/vault.js
+++ src/vault.js
@@ -4,31 +4,57 @@ const cp = require('child_process');
 
 module.exports = () => {
   const exec = cp.exec;
+  const execSync = cp.execSync;
   const env_variables = 'PATH='+process.env.PATH;
+  const { PATH, VAULT_ADDR, VAULT_CACERT } = process.env;
 
   return {
     get
   };
 
+  function runCommandSync(cmd) {
+    return execSync(cmd, {
+      env: {
+        PATH,
+        VAULT_ADDR,
+        VAULT_CACERT
+      },
+      encoding: 'utf-8'
+    });
+  }
+
+  function read(path) {
+    const cmd = `vault read secret/${path.replace(/^\/+/, '')}`; 
+    let stdout = null;
+
+    try {
+      stdout = runCommandSync(cmd);
+    } catch (e) {
+        console.log('Failed to read secret on path: ', path, '. Error: ', e);
+        throw e;
+    }
+
+    return stdout;
+  }
+
+  function list(path) {
+    const cmd = `vault list secret/${path}`; 
+    let stdout = null;
+
+    try {
+      stdout = runCommandSync(cmd);
+    } catch (e) {
+        return null;
+    }
+
+    return stdout;
+  }
+
   function get(path, cb) {
-    const cmd = `${env_variables} ./vlt ${path}`;
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        return cb(err, null);
-      }
+    let listOutput = list(path);
 
-      if (stdout.indexOf('Keys') !== 0) {
-        // Render secrets view
-        return cb(
-          null,
-          {
-            type: 'secrets',
-            content: stdout
-          }
-        );
-      }
-
-      const list = stdout.split('\n')
+    if (listOutput) {
+      const list = listOutput.split('\n')
         .filter((key) => {
           return key;
         })
@@ -43,6 +69,29 @@ module.exports = () => {
           content: list
         }
       );
-    });
+    }
+
+    // Try reading the path as a secret
+    //
+    // Detecting whether `path` leads to a directory
+    // of secrets or a secret by capturing exceptions 
+    // is not ideal but Vault doesn't make this information available.
+    if (!listOutput) {
+      let readOutput = null;
+      try {
+        readOutput = read(path);
+      } catch (e) {
+        return cb(e, null);
+      }
+
+      // Render secrets view
+      return cb(
+        null,
+        {
+          type: 'secrets',
+          content: readOutput
+        }
+      );
+    }
   }
 };
