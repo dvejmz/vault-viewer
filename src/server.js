129f01bdd1674e57a8d94b4702d054e8abbd7d9c
dvejmz <dvejmz@gmail.com>
2018-08-10 00:28:16 +0100

    List secrets via Vault API, remove all shell-based Vault commands

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const pug = require('pug');
 const vault = require('./vault')();
--- src/vault.js
+++ src/vault.js
@@ -1,17 +1,9 @@
-'use strict';
-
-const util = require('util');
-const cp = require('child_process');
-
 const options = {
   token: '',
 };
 const vault = require('node-vault')(options);
-process.env.DEBUG = 'node-vault';
 
 module.exports = () => {
-  const exec = cp.exec;
-  const execSync = cp.execSync;
   const env_variables = 'PATH='+process.env.PATH;
   const { PATH, VAULT_ADDR, VAULT_CACERT, HOME } = process.env;
 
@@ -19,24 +11,11 @@ module.exports = () => {
     get
   };
 
-  function runCommandSync(cmd) {
-    return execSync(cmd, {
-      env: {
-        HOME,
-        PATH,
-        VAULT_ADDR,
-        VAULT_CACERT
-      },
-      encoding: 'utf-8'
-    });
-  }
-
   function trimPath(path) {
     return path.replace(/^\/+/, '');
   }
 
   async function read(path) {
-    let value = null;
     try {
       const res = await vault.read(`secret/${trimPath(path)}`);
       return res.data;
@@ -47,21 +26,19 @@ module.exports = () => {
     return null;
   }
 
-  function list(path) {
-    const cmd = `vault list -format json secret/${trimPath(path)}`; 
-    let stdout = null;
-
+  async function list(path) {
     try {
-      stdout = runCommandSync(cmd);
+      const res = await vault.list(`secret/${trimPath(path)}`);
+      return res.data.keys;
     } catch (e) {
-        return null;
+      console.log(e.toString());
     }
 
-    return JSON.parse(stdout);
+    return null;
   }
 
   async function get(path) {
-    let listOutput = list(path);
+    let listOutput = await list(path);
 
     if (listOutput) {
       const list = listOutput
